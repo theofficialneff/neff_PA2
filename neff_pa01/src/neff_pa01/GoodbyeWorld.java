a828da30c2a5d02d3f94e69c118296d8a1504b16
theofficialneff <55040216+theofficialneff@users.noreply.github.com>
2020-01-18 20:17:00 -0500

    Assignment 1

---
+++ neff_pa01/src/neff_pa01/GoodbyeWorld.java
@@ -0,0 +1,9 @@
+package neff_pa01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("Goodbye, World!");
+	}
+}
---
+++ neff_pa01/src/neff_pa01/HelloWorld.java
@@ -0,0 +1,9 @@
+package neff_pa01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+	}
+}
